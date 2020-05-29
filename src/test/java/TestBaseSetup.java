f22d40bd662c6d37a6ae64c6a6a8c6858af99c69
Александр Арбузов <a.arbuzov@ukr.net>
2020-05-29 13:20:30 +0300

    Parallelization Gecko driver "stuck" AWS

--- src/test/java/TestBaseSetup.java
+++ src/test/java/TestBaseSetup.java
@@ -1,10 +1,13 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import utils.Screenshot;
 
 import java.net.MalformedURLException;
@@ -14,18 +17,36 @@ public class TestBaseSetup {
     WebDriver driver;
     Screenshot screenshot;
 
+
+    @Parameters({ "browser" })
     @BeforeMethod
-    public void beforeMethod(){
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-notifications");
-        options.addArguments("--window-size=1300,1080");
-        options.addArguments("--headless");
-        //driver = new ChromeDriver();
-        try {
-            driver = new RemoteWebDriver(new URL("http://ec2-18-191-164-149.us-east-2.compute.amazonaws.com:4444/wd/hub"), options);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
+    public void beforeMethod(String browser){
+        System.out.println("browser : " + browser);
+
+        if (browser.equals("firefox")){
+            FirefoxOptions options = new FirefoxOptions();
+            //System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
+            options.addArguments("--disable-notifications");
+            //options.addArguments("--window-size=1300,1080");
+            options.addArguments("--headless");
+            driver = new FirefoxDriver(options);
+            try {
+                driver = new RemoteWebDriver(new URL("http://ec2-18-191-164-149.us-east-2.compute.amazonaws.com:4444/wd/hub"), options);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        } else {
+            ChromeOptions options = new ChromeOptions();
+            //System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+            options.addArguments("--disable-notifications");
+            options.addArguments("--window-size=1300,1080");
+            options.addArguments("--headless");
+            //driver = new ChromeDriver(options);
+            try {
+                driver = new RemoteWebDriver(new URL("http://ec2-18-191-164-149.us-east-2.compute.amazonaws.com:4444/wd/hub"), options);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
         }
         screenshot = new Screenshot(driver);
     }
