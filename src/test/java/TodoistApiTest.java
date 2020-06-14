78c2aa1e2f55d279968858b0afa64ae11d3f5997
33152 <tegeran43>
2020-06-14 18:48:11 +0300

    Для сайта https://todoist.com/ покрыть тестами CRUD по функционалу задач проекта (насколько хватит времени и желания )

--- src/test/java/TodoistApiTest.java
+++ src/test/java/TodoistApiTest.java
@@ -1,76 +1,17 @@
 import com.github.javafaker.Faker;
 import io.qameta.allure.Description;
 import io.restassured.http.ContentType;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 import static io.restassured.RestAssured.given;
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
 
 public class TodoistApiTest {
 
     String todoistToken = "Bearer 44fb2d6a3d7ffe079a319a3c5ed7e152a135f6cb";
-    String fstPrjId = "2237883857";
     String name = "";
     long newTaskId;
-
-/*
-    @Test()
-    @Description("Testing projects by todoist api")
-    public void getP() {
-        Response response = given().
-                header("Authorization", todoistToken).
-                when().
-                get("https://api.todoist.com/rest/v1/projects");
-        response
-                .then()
-                .statusCode(200);
-        //System.out.println("response.body() : " +response.body().prettyPeek());
-        JsonPath path = response.jsonPath();
-        List<Map> list = path.getList("");
-        List<String> actualIDs = new ArrayList<>();
-        for (Map project : list) {
-            //System.out.println(project.get("id"));
-            actualIDs.add((String) project.get("id").toString());
-
-        }
-
-        assertTrue(actualIDs.contains(fstPrjId), "Needed project id '" + fstPrjId + "' isn't found");
-        //assertFalse(actualIDs.contains(fstPrjId), "Needed project id '" + fstPrjId + "' isn't found");
-    }
-*/
-    /*
-    @Test()
-    @Description("Testing active tasks in Project by todoist api")
-    public void getT() {
-        Response response = given().
-                header("Authorization", todoistToken).
-                when().
-                get("https://api.todoist.com/rest/v1/tasks");
-        JsonPath path = response.jsonPath();
-        List<Map> list = path.getList("");
-        List<String> actualTAskIDs = new ArrayList<>();
-        for (Map task : list) {
-            if (task.get("project_id").toString().equals(fstPrjId)) {
-                //System.out.println("Task Id : " + task.get("id"));
-                actualTAskIDs.add((String) task.get("id").toString());
-            }
-        }
-
-
-        System.out.println("actualTAskIDs : " + actualTAskIDs.size());
-        //assertTrue(actualIDs.contains(fstPrjId), "Needed project id '" + fstPrjId + "' isn't found");
-        //assertFalse(actualIDs.contains(fstPrjId), "Needed project id '" + fstPrjId + "' isn't found");
-        //System.out.println(response.getBody().peek());
-        assertEquals(actualTAskIDs.size(), 3, "Wrong  tasks amount. Expected '3' but found : " + actualTAskIDs.size());
-    }
-    */
-
 
     @Test(priority = 1)
     @Description("Create a new task")
@@ -176,4 +117,62 @@ public class TodoistApiTest {
         then().
                         statusCode(204);
     }
+
+    /*
+    @Test()
+    @Description("Testing projects by todoist api")
+    public void getP() {
+        Response response = given().
+                header("Authorization", todoistToken).
+                when().
+                get("https://api.todoist.com/rest/v1/projects");
+        response
+                .then()
+                .statusCode(200);
+        //System.out.println("response.body() : " +response.body().prettyPeek());
+        JsonPath path = response.jsonPath();
+        List<Map> list = path.getList("");
+        List<String> actualIDs = new ArrayList<>();
+        for (Map project : list) {
+            //System.out.println(project.get("id"));
+            actualIDs.add((String) project.get("id").toString());
+
+        }
+
+        assertTrue(actualIDs.contains(fstPrjId), "Needed project id '" + fstPrjId + "' isn't found");
+        //assertFalse(actualIDs.contains(fstPrjId), "Needed project id '" + fstPrjId + "' isn't found");
+    }
+*/
+    /*
+    @Test()
+    @Description("Testing active tasks in Project by todoist api")
+    public void getT() {
+        Response response = given().
+                header("Authorization", todoistToken).
+                when().
+                get("https://api.todoist.com/rest/v1/tasks");
+        JsonPath path = response.jsonPath();
+        List<Map> list = path.getList("");
+        List<String> actualTAskIDs = new ArrayList<>();
+        for (Map task : list) {
+            if (task.get("project_id").toString().equals(fstPrjId)) {
+                //System.out.println("Task Id : " + task.get("id"));
+                actualTAskIDs.add((String) task.get("id").toString());
+            }
+        }
+
+
+        System.out.println("actualTAskIDs : " + actualTAskIDs.size());
+        //assertTrue(actualIDs.contains(fstPrjId), "Needed project id '" + fstPrjId + "' isn't found");
+        //assertFalse(actualIDs.contains(fstPrjId), "Needed project id '" + fstPrjId + "' isn't found");
+        //System.out.println(response.getBody().peek());
+        assertEquals(actualTAskIDs.size(), 3, "Wrong  tasks amount. Expected '3' but found : " + actualTAskIDs.size());
+    }
+    */
+
+
+
+
+
+
 }
